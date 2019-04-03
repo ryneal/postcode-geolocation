9b6459bd2fa3bb62ebd861bc5b5d7d209020a872
ryan <ryandavidneal@gmail.com>
2019-04-03 21:23:21 +0100

    Added NumberUtil tests

--- src/test/java/com/github/ryneal/postcodegeolocation/util/NumberUtilTest.java
+++ src/test/java/com/github/ryneal/postcodegeolocation/util/NumberUtilTest.java
@@ -3,6 +3,7 @@ package com.github.ryneal.postcodegeolocation.util;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class NumberUtilTest {
@@ -16,21 +17,32 @@ public class NumberUtilTest {
 
     @Test
     public void shouldParseLongAsNullValue() {
+        Long nullLong = NumberUtil.parseLong(null);
+
+        assertThat(nullLong, is(nullValue()));
     }
 
     @Test(expected = NumberFormatException.class)
     public void shouldParseLongWithException() {
+        Long aLong = NumberUtil.parseLong("gfh");
     }
 
     @Test
     public void shouldParseDoubleSuccessfully() {
+        Double aDouble = NumberUtil.parseDouble("1.0");
+
+        assertThat(aDouble, is(1.0));
     }
 
     @Test
     public void shouldParseDoubleAsNullValue() {
+        Double nullDouble = NumberUtil.parseDouble(null);
+
+        assertThat(nullDouble, is(nullValue()));
     }
 
     @Test(expected = NumberFormatException.class)
     public void shouldParseDoubleWithException() {
+        Double aDouble = NumberUtil.parseDouble("fge");
     }
 }
