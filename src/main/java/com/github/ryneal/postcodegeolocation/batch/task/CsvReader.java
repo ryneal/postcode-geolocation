a287ed9847b8fa19827cc5704fad9cd5ed5a88a1
ryan <ryandavidneal@gmail.com>
2019-04-06 22:57:44 +0100

    Added File error handling

--- src/main/java/com/github/ryneal/postcodegeolocation/batch/task/CsvReader.java
+++ src/main/java/com/github/ryneal/postcodegeolocation/batch/task/CsvReader.java
@@ -2,17 +2,21 @@ package com.github.ryneal.postcodegeolocation.batch.task;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.annotation.AfterStep;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemReader;
 
 import javax.batch.api.listener.StepListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CsvReader implements ItemReader<Map<String, String>>, StepListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CsvReader.class);
     private static final String SPLIT_REGEX = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
     private final boolean hasHeader;
     private File file;
@@ -27,15 +31,19 @@ public class CsvReader implements ItemReader<Map<String, String>>, StepListener 
     @Override
     @BeforeStep
     public void beforeStep() throws Exception {
-        this.lineIterator = FileUtils.lineIterator(this.file);
-        if (hasHeader && this.lineIterator.hasNext()) {
-            this.headers = this.lineIterator.nextLine().split(SPLIT_REGEX);
+        try {
+            this.lineIterator = FileUtils.lineIterator(this.file);
+            if (hasHeader && this.lineIterator.hasNext()) {
+                this.headers = this.lineIterator.nextLine().split(SPLIT_REGEX);
+            }
+        } catch (FileNotFoundException e) {
+            LOGGER.error("File " + this.file.getPath() + " not found. Csv will not be read", e);
         }
     }
 
     @Override
     public Map<String, String> read() {
-        if (this.lineIterator.hasNext()) {
+        if (this.lineIterator != null && this.lineIterator.hasNext()) {
             Map<String, String> records = new HashMap<>();
             String[] entries = this.lineIterator.nextLine().split(SPLIT_REGEX);
             if (hasHeader && this.headers.length != entries.length) return records;
